e136cdfc71e335343d7d7d606c066e810c9e9246
Dian Bahnev <dbahnev@gmail.com>
2018-09-27 19:37:12 +0100

    Adding recuresion to reverse string solutions

--- src/reverse_string.py
+++ src/reverse_string.py
@@ -19,3 +19,21 @@ def reverse_string_for_loop(s):
         string[index], string[-index-1] = string[-index-1], string[index]
 
     return ''.join(string)
+
+
+def reverse_string_recursion_1(s):
+    if s == "":
+        return s
+
+    return s[-1] + reverse_string_recursion_1(s[:-1])
+
+
+def reverse_string_recursion_2(s):
+    if s == "":
+        return s
+    elif len(s) == 1:
+        return s
+    elif len(s) == 2:
+        return s[1] + s[0]
+
+    return s[-1] + reverse_string_recursion_2(s[1:-1]) + s[0]
--- tst/reverse_string.py
+++ tst/reverse_string.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.reverse_string import reverse_string_while_loop, reverse_string_for_loop
+from src.reverse_string import reverse_string_while_loop, reverse_string_for_loop, reverse_string_recursion_1, \
+    reverse_string_recursion_2
 
 
 class ReverseString(unittest.TestCase):
@@ -18,26 +19,53 @@ class ReverseString(unittest.TestCase):
         reversed_s = reverse_string_while_loop(self.string_even)
 
         # Assert
-        self.assertEqual(reversed_s, self.expected_even)
+        self.assertEqual(self.expected_even, reversed_s)
 
     def test_reverse_while_loop_odd(self):
         # Action
         reversed_s = reverse_string_while_loop(self.string_odd)
 
         # Assert
-        self.assertEqual(reversed_s, self.expected_odd)
+        self.assertEqual(self.expected_odd, reversed_s)
 
     def test_reverse_for_loop_even(self):
         # Action
         reversed_s = reverse_string_for_loop(self.string_even)
 
         # Assert
-        self.assertEqual(reversed_s, self.expected_even)
+        self.assertEqual(self.expected_even, reversed_s)
 
     def test_reverse_for_loop_odd(self):
         # Action
         reversed_s = reverse_string_for_loop(self.string_odd)
 
         # Assert
-        self.assertEqual(reversed_s, self.expected_odd)
+        self.assertEqual(self.expected_odd, reversed_s)
 
+    def test_reverse_recursion_1_even(self):
+        # Action
+        reversed_s = reverse_string_recursion_1(self.string_even)
+
+        # Assert
+        self.assertEqual(self.expected_even, reversed_s)
+
+    def test_reverse_recursion_1_odd(self):
+        # Action
+        reversed_s = reverse_string_recursion_1(self.string_odd)
+
+        # Assert
+        self.assertEqual(self.expected_odd, reversed_s)
+
+    def test_reverse_recursion_2_even(self):
+        # Action
+        reversed_s = reverse_string_recursion_2(self.string_even)
+
+        # Assert
+        self.assertEqual(self.expected_even, reversed_s)
+
+    def test_reverse_recursion_2_odd(self):
+        # Action
+        reversed_s = reverse_string_recursion_2(self.string_odd)
+
+        # Assert
+        self.assertEqual(self.expected_odd, reversed_s)
